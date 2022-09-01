09527d152a56e125dc14fca84a06e448fe8c8a86
Egor Antropov <antropov-zhora@mail.ru>
2022-09-01 12:07:00 +0300

    kdfjfkd

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//задача 47
+
+Console.WriteLine("Введите количество строк");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[lines, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < lines; i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+
+
+
